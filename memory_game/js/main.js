b08206e2fee8f3e910110690cc1d8fa720d467af
Emile Desfontaines <emile.desfontaines@gmail.com>
2019-06-04 19:45:12 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-console.log(cardsInPlay);
-
-if (cardsInPlay.length === 2) {
-	if (cardOne === cardTwo) {
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	console.log(cardsInPlay);
+	cardsInPlay.push(cards[cardId]);
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+}
+}
+
+flipCard(0);
+flipCard(1);
